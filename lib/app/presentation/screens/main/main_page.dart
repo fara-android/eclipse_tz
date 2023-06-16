f1cbdd407481f8573f11ee5f4d484f25cc92f195
fara-android <abdusatarovfaruh3@gmail.com>
2023-06-16 21:49:46 +0600

    main_page refactoring

--- lib/app/presentation/screens/main/main_page.dart
+++ lib/app/presentation/screens/main/main_page.dart
@@ -1,8 +1,12 @@
+import 'package:eclipse_tz/app/presentation/cubits/get_all_users/get_all_users_cubit.dart';
+import 'package:eclipse_tz/app/presentation/screens/main/components/user_card.dart';
 import 'package:eclipse_tz/data/models/user_model.dart';
 import 'package:eclipse_tz/data/services/api_service.dart';
 import 'package:eclipse_tz/app/presentation/theme/app_colors.dart';
 import 'package:eclipse_tz/app/presentation/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../service_locator.dart';
 import '../../widgets/loader.dart';
 import '../user/user_page.dart';
 
@@ -40,47 +44,42 @@ class _MainPageState extends State<MainPage> {
       ),
       body: _isLoading
           ? const Loader()
-          : ListView.separated(
-              padding: const EdgeInsets.all(16),
-              itemCount: result.length,
-              separatorBuilder: (_, __) => const SizedBox(
-                height: 12,
-              ),
-              itemBuilder: (context, index) {
-                final user = result[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push<void>(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => UserPage(
-                          userModel: user,
+          : BlocProvider(
+              create: (context) => sl<GetAllUsersCubit>()..getUsers(),
+              child: BlocBuilder<GetAllUsersCubit, GetAllUsersState>(
+                builder: (context, state) {
+                  return state.when(
+                    initial: () => const SizedBox(),
+                    loading: () => const Loader(),
+                    success: (users) {
+                      return ListView.separated(
+                        padding: const EdgeInsets.all(16),
+                        itemCount: users.length,
+                        separatorBuilder: (_, __) => const SizedBox(
+                          height: 12,
                         ),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    color: AppColors.gray,
-                    padding: const EdgeInsets.all(10),
-                    child: ListTile(
-                      horizontalTitleGap: 8,
-                      title: Text(
-                        user.username,
-                        style: AppTextStyles.title,
-                      ),
-                      subtitle: Text(
-                        user.name,
-                        style: AppTextStyles.subtitle,
-                      ),
-                      leading: const Icon(
-                        Icons.person,
-                        size: 32,
-                        color: AppColors.white,
-                      ),
-                    ),
-                  ),
-                );
-              },
+                        itemBuilder: (context, index) {
+                          return UserCard(
+                            onTap: () {
+                              Navigator.push<void>(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => UserPage(
+                                    userModel: users[index],
+                                  ),
+                                ),
+                              );
+                            },
+                            userName: users[index].username,
+                            name: users[index].name,
+                          );
+                        },
+                      );
+                    },
+                    failed: (message) => Text(message),
+                  );
+                },
+              ),
             ),
     );
   }
