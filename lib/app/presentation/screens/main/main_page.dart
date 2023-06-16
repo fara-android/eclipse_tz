796f8358998ff3861fbee9f4a002592e97c25ede
fara-android <abdusatarovfaruh3@gmail.com>
2023-06-16 21:51:19 +0600

    fix main page

--- lib/app/presentation/screens/main/main_page.dart
+++ lib/app/presentation/screens/main/main_page.dart
@@ -1,36 +1,16 @@
-import 'package:eclipse_tz/app/presentation/cubits/get_all_users/get_all_users_cubit.dart';
-import 'package:eclipse_tz/app/presentation/screens/main/components/user_card.dart';
-import 'package:eclipse_tz/data/models/user_model.dart';
-import 'package:eclipse_tz/data/services/api_service.dart';
-import 'package:eclipse_tz/app/presentation/theme/app_colors.dart';
-import 'package:eclipse_tz/app/presentation/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../../../service_locator.dart';
+import '../../cubits/get_all_users/get_all_users_cubit.dart';
+import '../../theme/app_colors.dart';
+import '../../theme/app_text_styles.dart';
 import '../../widgets/loader.dart';
 import '../user/user_page.dart';
+import 'components/user_card.dart';
 
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
-
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  List<UserModel> result = List.empty();
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      result = await ApiService.getAllUsers();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -42,45 +22,43 @@ class _MainPageState extends State<MainPage> {
         titleTextStyle: AppTextStyles.title,
         backgroundColor: AppColors.gray,
       ),
-      body: _isLoading
-          ? const Loader()
-          : BlocProvider(
-              create: (context) => sl<GetAllUsersCubit>()..getUsers(),
-              child: BlocBuilder<GetAllUsersCubit, GetAllUsersState>(
-                builder: (context, state) {
-                  return state.when(
-                    initial: () => const SizedBox(),
-                    loading: () => const Loader(),
-                    success: (users) {
-                      return ListView.separated(
-                        padding: const EdgeInsets.all(16),
-                        itemCount: users.length,
-                        separatorBuilder: (_, __) => const SizedBox(
-                          height: 12,
-                        ),
-                        itemBuilder: (context, index) {
-                          return UserCard(
-                            onTap: () {
-                              Navigator.push<void>(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => UserPage(
-                                    userModel: users[index],
-                                  ),
-                                ),
-                              );
-                            },
-                            userName: users[index].username,
-                            name: users[index].name,
-                          );
-                        },
-                      );
-                    },
-                    failed: (message) => Text(message),
-                  );
-                },
-              ),
-            ),
+      body: BlocProvider(
+        create: (context) => sl<GetAllUsersCubit>()..getUsers(),
+        child: BlocBuilder<GetAllUsersCubit, GetAllUsersState>(
+          builder: (context, state) {
+            return state.when(
+              initial: () => const SizedBox(),
+              loading: () => const Loader(),
+              success: (users) {
+                return ListView.separated(
+                  padding: const EdgeInsets.all(16),
+                  itemCount: users.length,
+                  separatorBuilder: (_, __) => const SizedBox(
+                    height: 12,
+                  ),
+                  itemBuilder: (context, index) {
+                    return UserCard(
+                      onTap: () {
+                        Navigator.push<void>(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => UserPage(
+                              userModel: users[index],
+                            ),
+                          ),
+                        );
+                      },
+                      userName: users[index].username,
+                      name: users[index].name,
+                    );
+                  },
+                );
+              },
+              failed: (message) => Text(message),
+            );
+          },
+        ),
+      ),
     );
   }
 }
